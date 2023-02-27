0129ff0122cc0e2896ea3baf50cf7671edace58e
joseph <jsensback@gmail.com>
2023-02-27 12:00:28 -0800

    Changed contact

--- frontend/src/screens/Contact.js
+++ frontend/src/screens/Contact.js
@@ -4,8 +4,8 @@ function Contact() {
   return (
     <div>
       <h1 className="border-bottom">Contact</h1>
-      <p>Email: info@meowsandco.com</p>
-      <p>Phone: (360) 913-1639</p>
+      <p>Email: JSensback@gmail.com</p>
+      <p>Phone: (954) 552-9556</p>
     </div>
   )
 }
